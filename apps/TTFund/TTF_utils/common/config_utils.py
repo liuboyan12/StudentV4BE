9d81e299f46e2712bdec7c054b5274626c1ec94f
aliunking <594303711@qq.com>
2020-05-14 18:00:33 +0800

    base

--- apps/TTFund/TTF_utils/common/config_utils.py
+++ apps/TTFund/TTF_utils/common/config_utils.py
@@ -3,7 +3,7 @@ import configparser
 
 
 class ConfigUtils(object):
-    def __init__(self, conf_path='../config/local_config.ini'):
+    def __init__(self, conf_path='../config/config.ini'):
         self.current_path = os.path.dirname(__file__)
         self.conf_path = os.path.join(self.current_path, conf_path)
         self.conf = configparser.ConfigParser()
@@ -13,11 +13,34 @@ class ConfigUtils(object):
         print(name, type(value), value)
 
     def read_test_def(self):
-        self.tprint('self.current_path', self.current_path)
-        self.tprint('self.conf_path', self.conf_path)
-        self.tprint('self.conf_data', self.conf_data)
+        # self.tprint('self.current_path', self.current_path)
+        # self.tprint('self.conf_path', self.conf_path)
+        # self.tprint('self.conf_data', self.conf_data)
+        pass
+
+    @property
+    def current_url(self):
+        '''
+            今日数据请求地址
+        '''
+        return self.conf.get('url', 'current_url')
+
+    @property
+    def all_code_url(self):
+        '''
+            所有可以查询的基金列表
+        '''
+        return self.conf.get('url', 'all_code_url')
+
+    @property
+    def full_info_url(self):
+        '''
+            基金详情
+        '''
+        return self.conf.get('url', 'full_info_url')
 
 
 if __name__ == '__main__':
     c = ConfigUtils()
     c.read_test_def()
+    print(c.current_url)
